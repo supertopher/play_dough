e6dc3524f65fa7011f8d96d2b596778a651acef5
supertopher <asimplechris@gmail.com>
2013-07-21 13:05:40 -0700

    Initial postgres db commit no test:unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayDough::Application.config.secret_token = '5a037570fc8d6d77f1c02a86bfc9ffc039a9fff910c2fbec5d2646373788e527a4c57513292a93db3a76a7f0b5c49882f4320a61f9bcfad5d802b235bf575321'
