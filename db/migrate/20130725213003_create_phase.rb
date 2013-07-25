8ef97d9f60b1f88a382a4c0afc31bae52f2c7269
supertopher <asimplechris@gmail.com>
2013-07-25 16:47:37 -0700

    Removed two bad migrations

--- db/migrate/20130725213003_create_phase.rb
+++
@@ -1,9 +0,0 @@
-class CreatePhase < ActiveRecord::Migration
-  def change
-    create_table :phase do |t|
-      t.integer :phase
-      t.string :location
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130725213239_create_event.rb
+++
@@ -1,8 +0,0 @@
-class CreateEvent < ActiveRecord::Migration
-  def change
-    create_table :event do |t|
-      t.string :title, :url, :description
-      t.timestamps
-    end
-  end
-end
