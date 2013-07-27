070f5ef8ea455a760aff69eec1f3c44b280d4a73
supertopher <asimplechris@gmail.com>
2013-07-26 18:27:58 -0700

    Added a rake file to parse yaml assignment database... challenges are assignments for the moment. Can be changes in the future

---
+++ app/models/assignment.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: assignments
+#
+#  id                :integer          not null, primary key
+#  actor_id          :integer
+#  challenge_unit_id :integer
+#  required          :boolean
+#  level             :integer
+#  name              :string(255)
+#  description       :text
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
+class Assignment < ActiveRecord::Base
+  attr_accessible :actor_id, :challenge_unit_id, :description, :level, :name, :required
+end
