ab26cc786b25619ca0a256eff0d10be3da5929e6
supertopher <asimplechris@gmail.com>
2013-07-29 13:32:50 -0700

    Live markdown preview enabled

--- app/assets/javascripts/live_markdown.js
+++ app/assets/javascripts/live_markdown.js
@@ -1,19 +1,23 @@
+
+
 $(function(){
   console.log("start live_markdown.js");
+  // var Showdown = require('showdown');
+  var converter = new Showdown.converter();
   // var textInput = $("#text-area");
   // var textOutput = $('#live_preview');
   $('.text_area').keyup(function(){
-    var output = markdown.toHTML($('.text_area').val());
+    var output = converter.makeHtml($('.text_area').val());
+    // output = output.replace(/&/g, '&amp;');
+    // output = output.replace(/</g, '&lt;');
+    // output = output.replace(/>/g, '&gt;');
+
+    output.replace(/>/g, ">\n");
+    output.replace(/</g, "\n<");
+    output.replace(/\n{2,}/g, "\n\n");
     $('.live_preview').html(output);
+    // .replace(/</g, "\n<").replace(/\n{2,}/g, "\n\n");
     console.log("keyup");
 
   });
-
-  // function Editor(input, output) {
-  //       this.update = function () {
-  //         output.innerHTML = markdown.toHTML(input.value);
-  //       };
-  //       input.editor = this;
-  //       this.update();
-  //     }
 });
