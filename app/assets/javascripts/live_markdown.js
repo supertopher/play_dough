2424a2e203f09d833535b132d3f8722608e26e70
supertopher <asimplechris@gmail.com>
2013-07-29 13:55:06 -0700

    Load markdown preview on edit

--- app/assets/javascripts/live_markdown.js
+++ app/assets/javascripts/live_markdown.js
@@ -1,23 +1,15 @@
 
 
 $(function(){
-  console.log("start live_markdown.js");
-  // var Showdown = require('showdown');
   var converter = new Showdown.converter();
-  // var textInput = $("#text-area");
-  // var textOutput = $('#live_preview');
-  $('.text_area').keyup(function(){
-    var output = converter.makeHtml($('.text_area').val());
-    // output = output.replace(/&/g, '&amp;');
-    // output = output.replace(/</g, '&lt;');
-    // output = output.replace(/>/g, '&gt;');
+  markParser();
+  function markParser (){
 
+    var output = converter.makeHtml($('.text_area').val());
     output.replace(/>/g, ">\n");
     output.replace(/</g, "\n<");
     output.replace(/\n{2,}/g, "\n\n");
     $('.live_preview').html(output);
-    // .replace(/</g, "\n<").replace(/\n{2,}/g, "\n\n");
-    console.log("keyup");
-
-  });
+  }
+  $('.text_area').keyup(markParser);
 });
