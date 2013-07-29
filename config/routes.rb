f3f670a637edccdfd37558313a8a0a4cd521d3d3
supertopher <asimplechris@gmail.com>
2013-07-29 09:57:35 -0700

    Fixed user_id and challenge_id for comments.  Changed comment styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ PlayDough::Application.routes.draw do
   root :to => 'home#index'
   resources   :phases
   resources   :events
-  resources   :challenges
+  resources   :challenges, :comments
   resources   :cohorts
   devise_for  :users, :controllers => { :registration => "registrations" }
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
