839d42cc9087fc6b10058331abdc8fcba2771795
James Fickel <jamesfickel@gmail.com>
2013-07-30 12:29:33 -0700

    deleted public/images folder and did some css work on the user dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,36 +26,8 @@ class HomeController < ApplicationController
         end
       end
     end
-    #make these helper methods: todays_events, events_by_cohort, cohort_wide_events
-  	# @all_phases = []
-  	# phase_count = Phase.all.count
-  	# all_events = Event.all
-  	# all_events.each do |event|
-  	# 	if event.phases.count == phase_count
-  	# 		@all_phases << event
-  	# 	end
-  	# end
-   #  @todays_events = []
-  	# all_events.each do |event|
-   #    if event.start === Date.today
-   #      @todays_events << event
-   #    end
-   #  end
-   #  @phases = Phase.all
-   #  @active_phases = []
-   #  @phases.each do |phase|
-   #    if phase.start < Date.today
-   #      @active_phases << phase
-   #    end
-   #  end
-   #  @all_phases
-   #  @todays_events
-   #  @active_phases
- end
-
-  # if current_user.cohort.current_phase_day.nil? && current_user.cohort.start_date < DateTime.now
-  # current_user.cohort.current_phase_day = DateTime.now - current_user.cohort.start_date
-
+    @days = [1,2,3,4,5,6,7]
+  end
 
   def show
   end
