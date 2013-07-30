c77cdd412c1766051c39015ff18a96f35d2a6d2a
supertopher <asimplechris@gmail.com>
2013-07-29 18:29:05 -0700

    Colored syntax for markdown deemed to big.  Back to smaller items

--- app/assets/javascripts/live_markdown.js
+++ app/assets/javascripts/live_markdown.js
@@ -1,14 +1,9 @@
-
-
 $(function(){
   var converter = new Showdown.converter();
   markParser();
   function markParser (){
 
     var output = converter.makeHtml($('.text_area').val());
-    output.replace(/>/g, ">\n");
-    output.replace(/</g, "\n<");
-    output.replace(/\n{2,}/g, "\n\n");
     $('.live_preview').html(output);
   }
   $('.text_area').keyup(markParser);
