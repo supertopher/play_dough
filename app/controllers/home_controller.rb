6ffab62dcbd5b2f31fa528f64ca25773c27a46dc
James Fickel <jamesfickel@gmail.com>
2013-07-30 09:56:25 -0700

    user dashboard now has events for the week

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,27 @@
 class HomeController < ApplicationController
   def index
-    if current_user
-      if current_user.cohort
-        phase_time = current_user.cohort.current_phase_day
-        current_user_events = current_user.cohort.phase.events
-        phase_number = current_user.cohort.phase.number
-        if phase_number == 2
-          phase_time[0] = phase_time.first - 3
-        elsif phase_number == 3
-          phase_time[0] = phase_time.first - 6
+    if current_user && current_user.cohort
+      phase_time = current_user.cohort.current_phase_day
+      current_user_events = current_user.cohort.phase.events
+      phase_number = current_user.cohort.phase.number
+      if phase_number == 2
+        phase_time[0] = phase_time.first - 3
+      elsif phase_number == 3
+        phase_time[0] = phase_time.first - 6
+      end
+      @events_for_today = []
+      current_user_events.each do |event|
+        event.event_times.each do |event_time|
+          if event_time.week.to_i == phase_time.first && event_time.day.to_i == phase_time.last
+            @events_for_today << [event, event_time]
+          end
         end
-        @events_for_today = []
-        current_user_events.each do |event|
-          event.event_times.each do |event_time|
-            if event_time.week.to_i == phase_time.first && event_time.day.to_i == phase_time.last
-              @events_for_today << [event, event_time]
-            end
+      end
+      @events_for_week = []
+      current_user_events.each do |event|
+        event.event_times.each do |event_time|
+          if event_time.week.to_i == phase_time.first
+            @events_for_week << [event, event_time]
           end
         end
       end
