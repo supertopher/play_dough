b0f799ca9c8745bee6428d8a28c3ad81440056bf
kvanderd <me@katharine.org>
2013-07-31 12:22:01 -0700

    cleaned up old files, added new factory for User admin, create events spec with: test for admin user ability to login and access events

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,16 @@ require 'factory_girl'
 require 'faker'
 
 FactoryGirl.define do
-  factory :user do
-    email     {Faker::Internet.email}
-    password  "password"
-    cohort
-  end
+	factory :user do
+		email     {Faker::Internet.email}
+		password  "password"
+		cohort
+	end
+
+
+	factory :admin, class: User do
+		email     {Faker::Internet.email}
+		password  "password"
+		staff 	  true
+	end
 end
