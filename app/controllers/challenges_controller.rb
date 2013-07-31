576560f3ce665a40bdc325f282a4e232990325ad
supertopher <asimplechris@gmail.com>
2013-07-31 14:34:51 -0700

    Randomized comment creation. Started and comented out JS upvote handling

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -11,7 +11,6 @@ class ChallengesController < ApplicationController
       end
     end
     return unless current_user && current_user.cohort
-
     # loop through all of the user's events and fill two
     # arrays, one for the day and one for the week
     phase_time = current_user.cohort.current_phase_day
