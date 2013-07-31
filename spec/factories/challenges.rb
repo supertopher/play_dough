f9c69e30e64a24b3fbd6c64c41b67aa5d52f88ee
supertopher <asimplechris@gmail.com>
2013-07-31 09:56:36 -0700

    added default day and week to challenge model.  commented out currently unused challenge_unit_id and level

--- spec/factories/challenges.rb
+++ spec/factories/challenges.rb
@@ -4,9 +4,11 @@ require 'faker'
 FactoryGirl.define do
   factory :challenge do
     actor_id          {rand(10)}
-    challenge_unit_id 69
+    # challenge_unit_id 69
     required          true
-    level             18
+    # level             18
+    default_week        1
+    default_day         1
     name              {Faker::Company.bs}
     description       {Faker::Lorem.paragraphs(3)}
   end
