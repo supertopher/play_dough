1be244d361236d12bdc09d8c12d57952eec60a02
supertopher <asimplechris@gmail.com>
2013-07-31 19:27:45 -0700

    commit for stash pop of JS changes

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,6 @@
 $(function(){
   $('.comment_submit').click(function(event){
-    if ($('#comment_body').val() === "")
+    ('#comment_body').val() === "")
     {
       event.preventDefault();
       $(".new_comment_form").append("<h3 class='error'>Please enter a comment.</h3>")
